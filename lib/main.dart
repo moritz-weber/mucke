0a960261336a3dca99104dee231f8a1d86f99c2a
Moritz Weber <moritz.weber@posteo.de>
2020-03-28 11:06:23 +0100

    force portrait mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_audio_query/flutter_audio_query.dart';
 import 'package:mosh/system/datasources/local_music_fetcher.dart';
 import 'package:mosh/system/datasources/moor_music_data_source.dart';
@@ -18,6 +19,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
+
     return MaterialApp(
       title: 'mosh',
       theme: theme(),
