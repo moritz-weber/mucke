bb80b344f5650db755ac09e3ce5fec57bf4ab1c0
Frieder Hannenheim <frieder.hannenheim@proton.me>
2023-07-12 18:02:44 +0200

    Add share song button

--- src/lib/presentation/widgets/song_bottom_sheet.dart
+++ src/lib/presentation/widgets/song_bottom_sheet.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/localizations.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:get_it/get_it.dart';
+import 'package:share_plus/share_plus.dart';
 
 import '../../domain/entities/album.dart';
 import '../../domain/entities/artist.dart';
@@ -135,6 +136,17 @@ class _SongBottomSheetState extends State<SongBottomSheet> {
                         style: TEXT_SMALL_SUBTITLE.copyWith(height: 1.2),
                       ),
                     ],
+                  ),
+                ),
+                SizedBox(
+                  height: 64.0,
+                  child: Center(
+                    child: IconButton(
+                      onPressed: () {
+                        Share.share('${song.artist} - ${song.title}');
+                      },
+                      icon: const Icon(Icons.share),
+                    )
                   ),
                 ),
                 SizedBox(
